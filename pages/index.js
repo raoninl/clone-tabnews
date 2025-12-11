01b2137ee06aed66127bf21cb0e15178532e0f01
Raoní Lemos <106082903+raoninl@users.noreply.github.com>
2025-12-11 03:51:31 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home(){
-    return <h1>Você é mais corajoso do que pensa, mais forte do que parece e mais esperto do que acredita.</h1>
+function Home() {
+  return (
+    <h1>
+      Você é mais corajoso do que pensa, mais forte do que parece e mais esperto
+      do que acredita.
+    </h1>
+  );
 }
 
 export default Home;
