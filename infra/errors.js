918442987a205f9055c3e545366096bb70ee17ea
Raoní Lemos <raoni_lemos@yahoo.com.br>
2026-02-17 01:34:36 -0300

    feat: create and use `ValidationError` in `controller.js`

--- infra/errors.js
+++ infra/errors.js
@@ -18,14 +18,34 @@ export class InternalServerError extends Error {
   }
 }
 
-export class ServerError extends Error {
+export class ServiceError extends Error {
   constructor({ cause, message }) {
     super(message || "Serviço indisponível no momento.", {
       cause,
     });
-    this.name = "ServerError";
+    this.name = "ServiceError";
     this.action = "Verifique se o serviço está disponível.";
     this.statusCode = 503;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class ValidationError extends Error {
+  constructor({ cause, message, action }) {
+    super(message || "Um erro de validação ocorreu.", {
+      cause,
+    });
+    this.name = "ValidationError";
+    this.action = action || "Ajuste os dados enviados e tente novamente.";
+    this.statusCode = 400;
   }
 
   toJSON() {
